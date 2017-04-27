3bcf14ed83251851f6b350b4bf7281aaff94ebaf
ruipeng <ruipeng@iflytek.com>
2017-04-27 15:37:54 +0800

    1. initial commit

---
+++ libA/src/main/java/com/pangxiezhou/liba/MainA.java
@@ -0,0 +1,9 @@
+package com.pangxiezhou.liba;
+
+/**
+ * Created by PR on 2017/4/27.
+ */
+
+public class MainA {
+
+}
---
+++ libB/src/main/java/com/pangxiezhou/libb/MainB.java
@@ -0,0 +1,8 @@
+package com.pangxiezhou.libb;
+
+/**
+ * Created by PR on 2017/4/27.
+ */
+
+public class MainB {
+}
